0249133932657c714a2cd54b058aa3881d077915
Дмитрий Филимонов <atrom@list.ru>
2022-01-11 21:05:24 +0300

    UPD: everything

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,25 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    discription = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'discription': discription
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     return HttpResponse(f'Группа постов {slug}')
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    discription = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'discription': discription
+    }
+    return render(request, template, context)
