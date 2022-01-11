2c5e3dab81027c78b3dbaec1e08b693c744e9bbb
Дмитрий Филимонов <atrom@list.ru>
2022-01-11 22:00:25 +0300

    UDP: migrations, created model Post

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,28 +1,25 @@
-from django.http import HttpResponse
+# posts/views.py
+
 from django.shortcuts import render
 
 
 def index(request):
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
-    discription = 'Это главная страница проекта Yatube'
+    description = 'Это главная страница проекта Yatube'
     context = {
         'title': title,
-        'discription': discription
+        'description': description
     }
     return render(request, template, context)
-
-
-def group_posts(request, slug):
-    return HttpResponse(f'Группа постов {slug}')
 
 
 def group_list(request):
     template = 'posts/group_list.html'
     title = 'Лев Толстой – зеркало русской революции.'
-    discription = 'Здесь будет информация о группах проекта Yatube'
+    description = 'Здесь будет информация о группах проекта Yatube'
     context = {
         'title': title,
-        'discription': discription
+        'description': description
     }
     return render(request, template, context)
